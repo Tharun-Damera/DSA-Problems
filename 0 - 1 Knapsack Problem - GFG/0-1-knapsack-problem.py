7bea3ca4628873dd678acb8cce672db531273484
Tharun Damera <89800924+Tharun-Damera@users.noreply.github.com>
2022-11-20 17:22:07 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.py
@@ -3,26 +3,26 @@
 class Solution:
     
     #Function to return max value that can be put in knapsack of capacity W.
-    def knapSack(self,W, wt, val, n):
+    def knapSack(self,w, wt, val, n):
        
-        dp = [[-1 for i in range(W + 1)]for j in range(n + 1)]
+        dp = [[-1 for i in range(w + 1)]for j in range(n + 1)]
        
-        def kS(wt, val, W, n):
-            if n == 0 or W== 0:
-                return 0
-            
-            if dp[n][W] != -1:
-                return dp[n][W]
+        for i in range(n + 1):
+            for j in range(w + 1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
                 
-            if wt[n-1] <= W:
-                dp[n][W] = max(val[n-1] + kS(wt, val, W-wt[n-1], n-1), kS(wt, val, W, n-1))
-                return dp[n][W]
-            else:
-                dp[n][W] = kS(wt, val, W, n-1)
-                return dp[n][W]
-        
-        kS(wt, val, W, n)
-        
+        for i in range(1, n + 1):
+            for j in range(1, w + 1):
+                
+                if wt[i-1] <= j:
+                    dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j])
+                
+                else:
+                    dp[i][j] = dp[i-1][j]
+    
+            
+            
         return dp[n][W]
 
 #{ 
