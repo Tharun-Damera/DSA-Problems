2f485ac21b1cf44067948a18cbe2107c3d82b859
Tharun Damera <89800924+Tharun-Damera@users.noreply.github.com>
2022-12-16 21:00:26 +0530

    Added solution - LeetHub

---
+++ Implement Stack using Linked List - GFG/implement-stack-using-linked-list.py
@@ -0,0 +1,56 @@
+class StackNode:
+    # Constructor to initialize a node
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class MyStack:
+    
+    def __init__(self):
+        self.top = None
+    
+    def push(self, data):
+        if self.top == None:
+            self.top = StackNode(data)
+            return
+        
+        newNode = StackNode(data)
+        newNode.next = self.top
+        self.top = newNode
+        
+
+    def pop(self):
+        if self.top == None:
+            return -1
+        
+        x = self.top
+        self.top = self.top.next
+        
+        return x.data
+        
+        
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = MyStack()
+        q = int(input())
+        q1 = list(map(int, input().split()))
+        i = 0
+        while(i < len(q1)):
+            if(q1[i] == 1):
+                s.push(q1[i + 1])
+                i = i + 2
+            elif(q1[i] == 2):
+                print(s.pop(), end=" ")
+                i = i + 1
+            elif(s.isEmpty()):
+                print(-1)
+        print()
+
+# } Driver Code Ends
